420708b5d919590f2d3756b649d9e447f526a2d4
Nabed Khan <nahidcoder@gmail.com>
2026-08-27 09:43:35 +0400

    Solve '1280. Students and Examinations' leetcode's problem

---
+++ 12-students-and-examinations.sql
@@ -0,0 +1,11 @@
+SELECT
+s.student_id,
+s.student_name,
+sb.subject_name,
+COUNT(e.student_id) AS attended_exams
+FROM Students s
+CROSS JOIN Subjects sb
+LEFT JOIN Examinations e
+ON s.student_id = e.student_id AND sb.subject_name = e.subject_name
+GROUP BY sb.subject_name, s.student_id
+ORDER BY s.student_id, sb.subject_name
