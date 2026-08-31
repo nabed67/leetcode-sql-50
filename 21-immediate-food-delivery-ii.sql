5481e87f144fb7118f49bc81ea98d3ce528453aa
Nabed Khan <nahidcoder@gmail.com>
2026-08-31 12:02:06 +0400

    Solve '1174. Immediate Food Delivery II' problem

---
+++ 21-immediate-food-delivery-ii.sql
@@ -0,0 +1,8 @@
+SELECT 
+    ROUND((SUM(order_date=customer_pref_delivery_date) / COUNT(customer_id)) * 100, 2) AS immediate_percentage 
+FROM Delivery 
+WHERE(customer_id, order_date) IN (
+    SELECT customer_id, MIN(order_date) AS first_order_date 
+    FROM Delivery
+    GROUP BY customer_id
+)
